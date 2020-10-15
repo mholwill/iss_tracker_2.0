4ecbe2922e8910f1e1f274eea2f3d7d094f7973f
mholwill <m_holwill@hotmail.co.uk>
2020-10-15 13:50:55 +0100

    added containers folder and refactored that app.js is rendering globecontainer

---
+++ containers/GlobeContainer.js
@@ -0,0 +1,3 @@
+import React, { Component } from 'react';
+import ReactGlobe from 'react-globe';
+
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactGlobe from 'react-globe';
+import GlobeContainer from './containers/GlobeContainer'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <section>
-        <ReactGlobe />
+        <GlobeContainer />
       </section>
     </div>
   );
